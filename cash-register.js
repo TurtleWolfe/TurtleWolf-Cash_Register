13f130fb970894dff40e68577ffb1cca183b8cdb
TurtleWolfe <jonpohlner@gmail.com>
2018-08-28 16:33:05 -0400

    test with psuedo code passing
    added should block of testing

--- cash-register.js
+++ cash-register.js
@@ -1,6 +1,7 @@
 function checkCashRegister(price, cash, cid) {
 
-var totalINdrawer = [cid];
+var totalINdrawer = [cid]; //sum the arrary
+  
 var changeDue = cash - price ;	
 if (cid == changeDue ) {
   var change = {status: "CLOSED", change: [cid]};
@@ -8,13 +9,15 @@ if (cid == changeDue ) {
   }
     
 else if (cid < changeDue || cid < changeDue ) {
-  //denominations do not compute//
+  //denominations do not compute as  in 2 quarters in drawer, not suffeicent to convert to a dime, nickle & pennies because they ain't in the drawer dog//
+  // move this lower in logical structure
 change = {status: "INSUFFICIENT_FUNDS", change: []};
 console.log(change);  
   }
 
 else {
- change = {status: "OPEN", change: [changeDue]};
+ change = {status: "OPEN", change: [changeDue]};//calculate denominations of change.. 
+  
  console.log(change);     
 }
   // Here is your change, ma'am.
@@ -23,3 +26,8 @@ else {
 
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); //should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
