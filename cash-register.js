4b84b40dbf17d4046631338d2d79c36e014a88ca
TurtleWolfe <jonpohlner@gmail.com>
2018-08-28 15:34:41 -0400

    pseudo code runs now

--- cash-register.js
+++ cash-register.js
@@ -1,33 +1,25 @@
 function checkCashRegister(price, cash, cid) {
-price
-cash
-cid
-var totalINdrawer = [cid array];
+
+var totalINdrawer = [cid];
 var changeDue = cash - price ;	
 if (cid == changeDue ) {
   var change = {status: "CLOSED", change: [cid]};
+  console.log(change);
   }
     
-else if (cid < changeDue || //denominations do not compute// ) {
-  var change = {status: "INSUFFICIENT_FUNDS", change: []};
+else if (cid < changeDue || cid < changeDue ) {
+  //denominations do not compute//
+change = {status: "INSUFFICIENT_FUNDS", change: []};
+console.log(change);  
   }
 
 else {
-  var change = {status: "OPEN", change: [changeDue]},;
-  }
+ change = {status: "OPEN", change: [changeDue]};
+ console.log(change);     
+}
   // Here is your change, ma'am.
   return change;
 }
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
