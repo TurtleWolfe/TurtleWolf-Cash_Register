3b0a4a25eb88d4a13ffe29ed50637c4e62f19896
TurtleWolfe <JonPohlner@gmail.com>
2019-10-19 14:52:22 -0400

    4 of 6, to a different 5 of 6

--- cash-register.js
+++ cash-register.js
@@ -1,10 +1,11 @@
+// @ts-check
 function everybodiesGoodEnoughForSomeChange (changeDue) {
 let romanString = ''; // let while loop equal funtion call
 while (changeDue > 0){
     CountingChangeDue();//end of switch
   }//end of while..
 //refactor while loop as anounoumous function?      
-console.log('55 romanString = ',romanString);
+console.log('7 romanString = ',romanString);
 return romanString;
 
   function CountingChangeDue() {
@@ -59,17 +60,18 @@ return romanString;
     }
   }
 //refactor while loop as anounoumous function?
-}//end of  function everybodiesGoodEnoughForSomeChange 
+}//end of  function everybodiesGoodEnoughForSomeChange
+
 function checkCashRegister(price, cash, cid) {
-    let total = cid.map(currency => currency[1]*100).reduce((total, amount) => total + amount);
+let total = cid.map(currency => currency[1]*100).reduce((total, amount) => total + amount);
 let totalINdrawer = total/100; //use reduce method to sum the 2nd property of an array, do not use a loop
 console.log('totalINdrawer = ',totalINdrawer);  
 let changeDue = cash - price ;
 totalINdrawer = total / 100;	
-let change = '';//set default return
-console.log('9 change = ', change);
-console.log('10 changeDue = ',changeDue);  
-console.log('11 totalINdrawer = ',totalINdrawer);  
+let change = {};//set default return
+console.log('72 change = ', change);
+console.log('73 changeDue = ',changeDue);  
+console.log('74 totalINdrawer = ',totalINdrawer);  
   if (totalINdrawer == changeDue ) {
   change = {status: "CLOSED", change: cid};
   }//end of if (totalINdrawer == changeDue )
@@ -79,32 +81,41 @@ console.log('11 totalINdrawer = ',totalINdrawer);
     else {// (totalINdrawer > changeDue)     ..so calculate change     
 //calculate change object here
 let romanString = everybodiesGoodEnoughForSomeChange(changeDue);
-console.log('78romanString = ',romanString);
-console.log('80 totalINdrawer = ',totalINdrawer);	
-console.log('81 changeDue = ',changeDue);
+console.log('82romanString = ',romanString);
+console.log('83 totalINdrawer = ',totalINdrawer);	
+console.log('84 changeDue = ',changeDue);
       
-        if (totalINdrawer > changeDue) {
-          //denominations not suffeicent, do not compute, as in 2 quarters in drawer, to convert to a dime, nickle & pennies)
-          change = {status: "INSUFFICIENT_FUNDS", change: []};
-          console.log('85 change  ',change);
-          } 
-        else {  //just give his money bruh
+//         if (totalINdrawer > changeDue) {
+//           //denominations not suffeicent, do not compute, as in 2 quarters in drawer, to convert to a dime, nickle & pennies)
+// //line 88, add a variable!          
+//           change = {status: "INSUFFICIENT_FUNDS", change: []};
+//           console.log('89 change  ',change);
+//           } 
+//         else {  //just give his money bruh
           change = {status: "OPEN", change: changeDue};//calculate denominations of change.. 
           console.log('89 change = ',change);
           }// end of else, give him money bruh     
-         }//end of else, give change as per calculation
-  console.log('L92 change = ', change);
+//      }//end of else, give change as per calculation
+  console.log('L96 change = ', change);
   return change;}  // Here is your change, ma'am.//end of function checkCashRegister(price, cash, cid)
-//console.log('3.26, 100');
-//checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); //{status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
-
-//console.log('19.5, 20');
-//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); //should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
-
-// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
-
-//console.log('19.5, 20');
-checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-// should return {status: "INSUFFICIENT_FUNDS", change: []}.
-
-//  checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
+// Passed
+//  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+  //should return an object.
+  
+  // checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+  // should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+  
+  // checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+  //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+  
+// Passed
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+  
+//  Passed
+//  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+  
+//  Passed
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  //should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
